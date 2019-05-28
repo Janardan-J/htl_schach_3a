de0380ecf5bcf11783c60727ff2fe4c4931f0d23
Bemoty <signup.bemoty@outlook.com>
2019-05-28 13:46:20 +0200

    Added constructor overloading for Spielfeld
    Da SpielfeldIO beim Einlesen ein neues Spielfeld generiert, wurde ein zweiter Konstruktur für das Spielfeld hinzugefügt, welcher standardmäßig Team 1 starten lässt.

--- schach3a2/src/org/htlanich/schach/SpielFeld.java
+++ schach3a2/src/org/htlanich/schach/SpielFeld.java
@@ -1,56 +1,60 @@
 package org.htlanich.schach;
 
 public class SpielFeld {
-	private Feld[][] mat = new Feld[8][8];
-	private boolean werAmZug;
 
-	public SpielFeld(Feld[][] mat, boolean werAmZug) {
-		this.mat = mat;
-		this.werAmZug = werAmZug;
-	}
+    private Feld[][] mat;
+    private boolean werAmZug;
 
-	public Feld[][] getMat() {
-		return mat;
-	}
+    public SpielFeld( Feld[][] mat ) {
+        this(mat, true);
+    }
 
-	public void setMat(Feld[][] mat) {
-		this.mat = mat;
-	}
+    public SpielFeld( Feld[][] mat, boolean werAmZug ) {
+        this.mat = mat;
+        this.werAmZug = werAmZug;
+    }
 
-	public boolean isWerAmZug() {
-		return werAmZug;
-	}
+    public Feld[][] getMat() {
+        return mat;
+    }
 
-	public void setWerAmZug(boolean werAmZug) {
-		this.werAmZug = werAmZug;
-	}
-	
-	//Ausgabe geändert
-	public void ausgabe() {
-		for(int i=0;i<mat.length;i++) {
-			for(int j=0;j<mat.length;j++) {
-				String s= mat[i][j].toString();
-				System.out.print("|"+s);
-			}
-			System.out.println("|");
-		}
-	}
+    public void setMat( Feld[][] mat ) {
+        this.mat = mat;
+    }
 
-	public boolean schach() {
-		return false;
-	}
+    public boolean isWerAmZug() {
+        return werAmZug;
+    }
 
-	public boolean schachMatt() {
-		return false;
-	}
+    public void setWerAmZug( boolean werAmZug ) {
+        this.werAmZug = werAmZug;
+    }
 
-	public boolean spielzug(String zug) {
-		return false;
-	}
+    //Ausgabe geändert
+    public void ausgabe() {
+        for ( Feld[] aMat : mat ) {
+            for ( int j = 0; j < mat.length; j++ ) {
+                String s = aMat[j].toString();
+                System.out.print( "|" + s );
+            }
+            System.out.println( "|" );
+        }
+    }
 
-	public Position schach2koordinate(String schach) {
-		return null;
-	}
+    public boolean schach() {
+        return false;
+    }
+
+    public boolean schachMatt() {
+        return false;
+    }
+
+    public boolean spielzug( String zug ) {
+        return false;
+    }
+
+    public Position schach2koordinate( String schach ) {
+        return null;
+    }
+
 }
-//hi Josh
-//Lg Nof4ll
