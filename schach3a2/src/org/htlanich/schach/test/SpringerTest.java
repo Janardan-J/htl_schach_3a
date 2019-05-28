94c8e0779775e731d9d55d95fc6f63579ad6ad62
Bemoty <signup.bemoty@outlook.com>
2019-05-28 14:25:02 +0200

    Reformat SpringerTest

--- schach3a2/src/org/htlanich/schach/test/SpringerTest.java
+++ schach3a2/src/org/htlanich/schach/test/SpringerTest.java
@@ -9,49 +9,49 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 
 class SpringerTest {
-	SpielFeld f1 = null;
-	SpielFeld f2 = null;
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		f1 = Spielfeldio.einlesen("testfelder/S1.txt");
-		f2 = Spielfeldio.einlesen("testfelder/S2_waehrendSpiel.txt");
-	}
 
-	@AfterEach
-	void tearDown() throws Exception 
-	{
-	}
+    private SpielFeld f1 = null;
+    private SpielFeld f2 = null;
 
-	@Test
-	void testStartpositionNichtOk1() 
-	{
-		Springer s1 = (Springer)f1.getFeld(0, 1);
-		boolean nichtOk1 = s1.spielzugMoeglich(f1, new Position(0,1), new Position(0,2));
-		Assert.assertFalse(nichtOk1);
-	}
-	
-	@Test
-	void testStartpositionNichtOk2() 
-	{
-		Springer s1 = (Springer)f2.getFeld(4, 5);
-		boolean nichtOk2 = s1.spielzugMoeglich(f2, new Position(4,5), new Position(2,4));
-		Assert.assertFalse(nichtOk2);
-	}
+    @BeforeEach
+    void setUp() throws FileNotFoundException {
+        this.f1 = Spielfeldio.einlesen("testfelder/S1.txt");
+        this.f2 = Spielfeldio.einlesen("testfelder/S2_waehrendSpiel.txt");
+    }
 
-	@Test
-	void testStartpositionOk1() {
-		Springer s1 = (Springer)f1.getFeld(0, 1);
-		boolean ok1 = s1.spielzugMoeglich(f1, new Position(0,1), new Position(2,2));
-		Assert.assertTrue(ok1);
-	}
-	
-	@Test
-	void testStartpositionOk2() {
-		Springer s1 = (Springer)f2.getFeld(4, 5);
-		boolean ok2 = s1.spielzugMoeglich(f2, new Position(4,5), new Position(2,6));
-		Assert.assertTrue(ok2);
-	}
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testStartpositionNichtOk1() {
+        Springer s1 = (Springer) f1.getFeld(0, 1);
+        boolean nichtOk1 = s1.spielzugMoeglich(f1, new Position(0, 1), new Position(0, 2));
+        Assert.assertFalse(nichtOk1);
+    }
+
+    @Test
+    void testStartpositionNichtOk2() {
+        Springer s1 = (Springer) f2.getFeld(4, 5);
+        boolean nichtOk2 = s1.spielzugMoeglich(f2, new Position(4, 5), new Position(2, 4));
+        Assert.assertFalse(nichtOk2);
+    }
+
+    @Test
+    void testStartpositionOk1() {
+        Springer s1 = (Springer) f1.getFeld(0, 1);
+        boolean ok1 = s1.spielzugMoeglich(f1, new Position(0, 1), new Position(2, 2));
+        Assert.assertTrue(ok1);
+    }
+
+    @Test
+    void testStartpositionOk2() {
+        Springer s1 = (Springer) f2.getFeld(4, 5);
+        boolean ok2 = s1.spielzugMoeglich(f2, new Position(4, 5), new Position(2, 6));
+        Assert.assertTrue(ok2);
+    }
+
 }
