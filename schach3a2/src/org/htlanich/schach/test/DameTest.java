9677ecfb710b8a3e660daac35b3bae2c56518503
Daniel <Daniel@DESKTOP-NM00LLH>
2019-05-28 14:30:35 +0200

    Classe Dame Test erstellt + 4 Möglichkeiten => D1.txt erstellt

---
+++ schach3a2/src/org/htlanich/schach/test/DameTest.java
@@ -0,0 +1,50 @@
+package org.htlanich.schach.test;
+
+import org.htlanich.schach.figur.Dame;
+import org.htlanich.schach.Position;
+import org.htlanich.schach.SpielFeld;
+import org.htlanich.schach.Spielfeldio;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+class DameTest {
+	SpielFeld f1= null;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		f1= Spielfeldio.einlesen("testfelder/D1.txt");
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+
+	@Test
+	void testSpielzugMoeglich1() {
+		Dame d= (Dame)f1.getFeld(5, 7);
+		boolean m1 = d.spielzugMoeglich(f1, new Position(5,7),new Position(4,5));
+		Assert.assertTrue(m1);
+	}
+	@Test
+	void testSpielzugMoeglich2() {
+		Dame d= (Dame)f1.getFeld(5, 7);
+		boolean m1 = d.spielzugMoeglich(f1, new Position(5,7),new Position(6,7));
+		Assert.assertFalse(m1);
+	}
+	@Test
+	void testSpielzugMoeglich3() {
+		Dame d= (Dame)f1.getFeld(5, 1);
+		boolean m1 = d.spielzugMoeglich(f1, new Position(5,1),new Position(3,8));
+		Assert.assertTrue(m1);
+	}
+	@Test
+	void testSpielzugMoeglich4() {
+		Dame d= (Dame)f1.getFeld(5, 1);
+		boolean m1 = d.spielzugMoeglich(f1, new Position(5,1),new Position(5,2));
+		Assert.assertFalse(m1);
+	}
+
+}
