398bb70349f35d8115509883360a226649c0c3d6
Bemoty <signup.bemoty@outlook.com>
2019-06-04 14:27:32 +0200

    Position check object type + docs

--- schach3a2/src/org/htlanich/schach/Position.java
+++ schach3a2/src/org/htlanich/schach/Position.java
@@ -1,18 +1,28 @@
 package org.htlanich.schach;
 
-public class Position 
-{
-	public int x;
-	public int y;
-	
-	public Position(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+/**
+ * Positions-Klasse, um eine Position mit X- und Y-Koordinaten auf dem Spielfeld darzustellen
+ *
+ * @author albert
+ * @since 14.05.2019
+ */
+public class Position {
 
-	@Override
-	public boolean equals(Object obj) {
-		Position p = (Position)obj;
-		return x == p.x && y == p.y;
-	}
+    public int x;
+    public int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Position) {
+            Position p = (Position) obj;
+            return x == p.x && y == p.y;
+        }
+        return false;
+    }
+
 }
