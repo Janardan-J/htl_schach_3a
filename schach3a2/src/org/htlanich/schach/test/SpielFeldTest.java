fd36d51f0d9b76bd06fe2d07f0aea588d3a9ef39
Thomasa <Thomasa@10.10.218.86>
2019-06-04 14:12:32 +0200

    testSchach + testSchachMatt hinzugefügt

--- schach3a2/src/org/htlanich/schach/test/SpielFeldTest.java
+++ schach3a2/src/org/htlanich/schach/test/SpielFeldTest.java
@@ -2,9 +2,12 @@ package org.htlanich.schach.test;
 
 import org.htlanich.schach.SpielFeld;
 import org.htlanich.schach.SpielFeldIO;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertFalse;
 
 import java.io.FileNotFoundException;
 
@@ -14,7 +17,7 @@ class SpielFeldTest {
 
     @BeforeEach
     void setUp() throws FileNotFoundException {
-        this.start = SpielFeldIO.einlesen("start.txt");
+        this.start = SpielFeldIO.einlesen("testfelder/Sp1.txt");
     }
 
     @AfterEach
@@ -24,6 +27,18 @@ class SpielFeldTest {
     @Test
     void testAusgabeStart() {
         start.ausgabe();
+    }
+    
+    @Test
+    void testSchach() {
+        boolean m1 = start.schach();
+        Assert.assertTrue(m1);
+    }
+    
+    @Test
+    void testSchachMatt() {
+        boolean m1 = start.schachMatt();
+        Assert.assertFalse(m1);
     }
 
 }
