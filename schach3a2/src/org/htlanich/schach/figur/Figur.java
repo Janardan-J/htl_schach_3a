62f17aecf15f9b1601d695d4a743c6d6710f741e
Bemoty <signup.bemoty@outlook.com>
2019-06-04 14:19:20 +0200

    Added docs to Figur

--- schach3a2/src/org/htlanich/schach/figur/Figur.java
+++ schach3a2/src/org/htlanich/schach/figur/Figur.java
@@ -14,6 +14,11 @@ public abstract class Figur extends Feld {
     private boolean farbeWeiss;
     private boolean bewegt;
 
+    /**
+     * Instanziert eine Figur
+     * @param farbeWeiss true, wenn die Farbe dieser Figur weiß sein soll
+     * @param bewegt true, wenn die Figur bereits bewegt wurde
+     */
     public Figur(boolean farbeWeiss, boolean bewegt) {
         this.farbeWeiss = farbeWeiss;
         this.bewegt = bewegt;
@@ -35,8 +40,24 @@ public abstract class Figur extends Feld {
         this.bewegt = bewegt;
     }
 
+    /**
+     * Führt einen Spielzug von einer Position auf dem Spielfeld zu einer anderen aus
+     *
+     * @param sp Das Spielfeld auf dem die Aktion durchgeführt werden soll
+     * @param von die Position an der die Figur war
+     * @param nach die Position, an die die Figur verschoben werden soll
+     * @return true, wenn der Spielzug erfolgreich war, false wenn nicht
+     */
     public abstract boolean spielZug(SpielFeld sp, Position von, Position nach);
 
+    /**
+     * Prüft, ob die Figur sich von einer Position auf dem Spielfeld zu einer anderen bewegen kann
+     *
+     * @param sp Das Spielfeld auf dem die Aktion durchgeführt werden soll
+     * @param von die Position an der die Figur war
+     * @param nach die Position, an die die Figur verschoben werden soll
+     * @return true, wenn der Spielzug möglich ist, false wenn nicht
+     */
     public abstract boolean spielzugMoeglich(SpielFeld sp, Position von, Position nach);
 
     @Override
